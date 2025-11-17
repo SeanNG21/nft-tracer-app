6d758945c4b5f6ecc9d9e284ca672a4d24e45f90
SeanNG21 <nguyenviethung2192003@gmail.com>
2025-11-18 04:46:13 +0700

    fix

--- backend/integrations/__init__.py
+++ backend/integrations/__init__.py
@@ -1,6 +1,6 @@
 """Integration modules for NFTables and multi-function tracing"""
 
 from .nftables_manager import NFTablesManager
-from .multi_function_integration import *
+
 
 __all__ = ['NFTablesManager']
